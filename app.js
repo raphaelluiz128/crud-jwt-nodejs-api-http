3f63632c55e549eb34ee9c4680de1dd501a634c4
raphael luiz schettino <raphaelluiz128@hotmail.com>
2021-09-19 14:34:41 -0300

    Inclusão de cors, body, parser e tratamento de erros.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/users')
-const addressRoutes = require('./routes/address')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/users');
+const addressRoutes = require('./routes/address');
+
+app.user(morgan('dev'));
+app.user(bodyParser.urlencoded({extended : false}))
+app.user(bodyParser.json());
+
+app.user((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-Type', 'Accept', 'Authorization')
+})
 
 app.use('/users', usersRouter);
 app.use('/address', addressRouter);
 
+app.use((req, res, next) => {
+    const error = new Error("Não encontrado");
+    error.status = 404;
+    next(error);
+});
+
+app.user((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        error: {
+            mensagem: error.message
+        }
+    })
+});
 
 module.exports = app;
--- routes/address.js
+++ routes/address.js
@@ -5,6 +5,20 @@ router.get('/',(req, res, next) => {
     res.status(200).send({ mensagem : "ok, funcionou get address"})
 });
 
+router.get('/user/:id_user',(req, res, next) => {
+    const id_user = req.params.id_user;
+    res.status(200).send({ 
+        id: id_user,
+        mensagem : "ok, funcionou get address"})
+});
+
+router.get('/:id_address',(req, res, next) => {
+    const id_address = req.params.id_address;
+    res.status(200).send({ 
+        id: id_address,
+        mensagem : "ok, funcionou get address"})
+});
+
 router.post('/',(req, res, next) => {
     res.status(201).send({ mensagem : "ok, funcionou post address"})
 });
