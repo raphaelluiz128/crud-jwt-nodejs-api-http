60b917ba65fa9cb36d8814c8a45c5b3af6cdde67
raphael luiz schettino <raphaelluiz128@hotmail.com>
2021-09-19 20:00:09 -0300

    Criação de login e uso de jwt para criação de token

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const jwt =  require('jsonwebtoken');
 
 router.get('/',(req, res, next) => {
     res.status(200).send({ mensagem : "ok, funcionou get users"})
 });
 
+router.post('/login',(req, res, next) => {
+    mysql.getConnection((error, connection) => 
+    {
+        if(error) {
+            return res.status(500).send({ error : error})
+        }
+        const query = 'SELECT * FROM users where email = ?';
+        connection.query(query,[req.body.email],(error, results, fields) => {
+            connection.release();
+            if(error) {
+                return res.status(500).send({ error : error});
+            }
+            if(results.length === 0) {
+                return res.status(404).send({message: "e-mail não encontrado"});
+            }
+            // abaixo eu poderia usar uma variável de ambiente também
+            if( req.body.password === results[0].password) {
+                const token = jwt.sign({
+                    id_user: results[0].id_user,
+                    email: results[0].email
+                }, 'segredoDoJWT',
+                {
+                    expiresIn: "2h"
+                });
+                return res.status(200).send({message: "Login ok", token: token});
+
+            } else {
+                return res.status(401).send({message: "Falha na autenticação"});
+            }
+        });
+    });
+});
 
 router.post('/',(req, res, next) => {
     console.log('post user')
